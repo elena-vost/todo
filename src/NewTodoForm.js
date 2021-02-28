2e385a39517669d4e5391c6d51dfff955a528262
elena-vost <angara1298@gmail.com>
2021-02-28 22:25:21 +0100

    cheng1e31

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -31,7 +31,7 @@ class NewTodoForm extends Component {
           value={this.state.task}
           onChange={this.handleChange}
         />
-        <button>Add Todo</button>
+        <button>Add Todo!</button>
       </form>
     );
   }
